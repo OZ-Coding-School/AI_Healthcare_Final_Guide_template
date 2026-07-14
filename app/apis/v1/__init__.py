a38de326ca80ffc51f84a5e9d0f0e57312396b3b
Meoyoug <eodyd426@gmail.com>
2026-07-14 14:51:05 +0900

    ✨ feat: 건강검진 내역 등록, 목록조회, 상세조회, 삭제 API Router 추가

--- app/apis/v1/__init__.py
+++ app/apis/v1/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.apis.v1.annual_health_screening_apis import annual_health_screening_router
 from app.apis.v1.auth_routers import auth_router
 from app.apis.v1.blood_sugar_measurement_routers import blood_sugar_measurement_router
 from app.apis.v1.health_profile_routers import health_profile_router
@@ -12,3 +13,4 @@ v1_routers.include_router(user_router)
 v1_routers.include_router(health_profile_router)
 v1_routers.include_router(health_survey_router)
 v1_routers.include_router(blood_sugar_measurement_router)
+v1_routers.include_router(annual_health_screening_router)
